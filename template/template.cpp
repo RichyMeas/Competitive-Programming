da155eff322a3e5d116d747bb1b45ba873d33eb9
RichyMeas <61563986+RichyMeas@users.noreply.github.com>
2020-11-29 23:02:03 -0500

    First template version
    Edited version from Rachit Jain (https://github.com/rachitiitr)

---
+++ template/template.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+
+int mpow(int base, int exp); 
+
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+
+vi g[N]; //for graphs
+int a[N]; //for inputs
+
+void solve() {
+  int i, j, n, m;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    cin >> t; //comment out for 1 test case
+    while(t--) {
+      solve();
+    }
+
+    return 0;
+}
+
+int mpow(int base, int exp) {
+  base %= mod;
+  int result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = ((ll)result * base) % mod;
+    base = ((ll)base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
